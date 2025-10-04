3779e82694b24c560316446643ff1657c0b43e96
Denis Udila <razvanidenisudila@gmail.com>
2025-10-04 21:10:38 +0300

    adding asteroid spown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -14,11 +16,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(x, y, PLAYER_RADIUS)
-
-
+    asteroid = AsteroidField()
 
     while(True):
         dt = clock.tick(60) / 1000
